4234f80445866f1ce06249824c69002deccb6362
Asif <49162687+nhAsif@users.noreply.github.com>
2025-09-23 19:46:50 +0600

    Alioth: add flags for luna

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -15,6 +15,10 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_USES_MIUI_CAMERA := true
 TARGET_INCLUDES_MIUI_CAMERA := true
 
+TARGET_OPTIMIZED_DEXOPT := true
+WITH_GMS := true
+TARGET_USES_OMNI_GAPPS :=true
+
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
