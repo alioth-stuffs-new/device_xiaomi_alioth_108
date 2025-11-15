6c4369bd6104e60136897bf08d97bbc3dd1d4f51
nhAsif <najmulhasan3609@gmail.com>
2025-11-15 12:49:32 +0000

    evox bringup

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -28,3 +28,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=google/husky_beta/husky:16/BP41.250822.010/14082742:user/release-keys
+
+TARGET_USES_PICO_GAPPS := true
+
+# GameBar Performance Overlay
+$(call inherit-product, packages/apps/GameBar/gamebar.mk)
